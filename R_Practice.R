2f8bad6c5ba171fb71632650ae4606aaea787e61
HimikaRastogi <hrastogi.06@gmail.com>
2015-10-19 13:53:09 -0700

    First commit

---
+++ R_Practice.R
@@ -0,0 +1,16 @@
+# Box office Star Wars: In Millions (!) 
+# Construct matrix 
+box_office_all <- c(461, 314.4, 290.5, 247.9, 309.3, 165.8)
+movie_names <- c("A New Hope","The Empire Strikes Back","Return of the Jedi")
+col_titles <- c("US","non-US")
+star_wars_matrix <- matrix(box_office_all, nrow = 3, byrow = TRUE, dimnames = list(movie_names, col_titles))
+ticket_prices_matrix <- matrix(c(5, 5, 6, 6, 7, 7), nrow = 3, byrow = TRUE, dimnames = list(movie_names, col_titles)) 
+
+# Estimated number of visitors
+visitors <- star_wars_matrix / ticket_prices_matrix
+
+# Average number of US visitors
+average_us_visitors <- mean((star_wars_matrix[,1]) / (ticket_prices_matrix[,1]))
+
+# Average number of non-US visitors
+average_non_us_visitors <- mean((star_wars_matrix[,2]) / (ticket_prices_matrix[,2]))
