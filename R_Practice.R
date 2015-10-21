dd738488c216cccde24993a1306e527b43f87c7f
HimikaRastogi <hrastogi.06@gmail.com>
2015-10-21 11:09:38 -0700

    R Practice

--- R_Practice.R
+++ R_Practice.R
@@ -14,3 +14,35 @@ average_us_visitors <- mean((star_wars_matrix[,1]) / (ticket_prices_matrix[,1]))
 
 # Average number of non-US visitors
 average_non_us_visitors <- mean((star_wars_matrix[,2]) / (ticket_prices_matrix[,2]))
+
+# Assign to the variable 'theory' what this chapter is about!
+theory <- "R uses factors for categorical variables!"
+
+gender_vector <- c("Male", "Female", "Female", "Male", "Male")
+
+# Define factor_gender_vector using 'factor()'
+factor_gender_vector <- factor(gender_vector)
+
+animals_vector <- c("Elephant", "Giraffe", "Donkey", "Horse")
+temperature_vector <- c("High", "Low", "High","Low", "Medium")
+
+factor_animals_vector <- factor(animals_vector)
+factor_animals_vector
+factor_temperature_vector <- factor(temperature_vector, order = TRUE, levels = c("Low", "Medium", "High"))
+factor_temperature_vector
+
+speed_vector <- c("Fast", "Slow", "Slow", "Fast", "Ultra-fast") 
+
+# Add code below to order and give level to the factor
+factor_speed_vector <-  factor(speed_vector, ordered=TRUE, levels = c("Slow","Fast","Ultra-fast"))
+factor_speed_vector
+summary(factor_speed_vector) 
+
+speed_vector <- c("Fast", "Slow", "Slow", "Fast", "Ultra-fast")
+factor_speed_vector <- factor(speed_vector, ordered = TRUE, levels = c("Slow", "Fast", "Ultra-fast"))
+
+# Your code below
+compare_them <- factor_speed_vector[2] > factor_speed_vector[5] 
+
+# Is data analyst 2 faster than data analyst 5?
+compare_them
