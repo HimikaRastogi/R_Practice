c0bb904801df613f35f5858172a2ecfb2c9b153a
Himika Rastogi <hrastogi.06@gmail.com>
2015-10-25 01:44:33 -0700

    Update R_Practice.R

--- R_Practice.R
+++ R_Practice.R
@@ -41,7 +41,7 @@ summary(factor_speed_vector)
 speed_vector <- c("Fast", "Slow", "Slow", "Fast", "Ultra-fast")
 factor_speed_vector <- factor(speed_vector, ordered = TRUE, levels = c("Slow", "Fast", "Ultra-fast"))
 
-# Your code below
+# code below
 compare_them <- factor_speed_vector[2] > factor_speed_vector[5] 
 
 # Is data analyst 2 faster than data analyst 5?
